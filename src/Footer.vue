af31b4c49a16b5d7ef9862091d18e3c32fdc77bb
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-05 12:42:34 -0700

    Footer updated

---
+++ src/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <footer class="py-3 bg-light fixed-bottom">
+            <div class="container">
+                <p class="m-0 text-center">Copyright &copy; <a href="http://kateyeeum.com">Kate Lee</a> 2017</p>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default{}
+</script>
+
+<style>
+
+    footer,
+    a{
+        color: #595959;
+    }
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Navigation from './Navigation.vue'
 import BBC from './BBC.vue'
+import Footer from './Footer.vue'
 
 new Vue({
   el: '#app',
 
     components:{
         'kt-nav' : Navigation,
-        'kt-bbc' : BBC
+        'kt-bbc' : BBC,
+        'kt-footer' : Footer
     }
 })
