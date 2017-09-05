53731b9ed815acc306b50a137954bc35091d9a30
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-05 16:05:00 -0700

    TIME news

---
+++ src/TIME.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="row">
+        <div class="card" style="width: 20rem;" v-for="article in articles">
+
+            <img class="card-img-top" v-bind:src="article.urlToImage" alt="Card image cap">
+            <div class="card-block">
+                <h4 class="card-title"><a v-bind:href="article.url" target="_blank">{{article.title}}</a></h4>
+                <p>{{article.publishedAt}}</p>
+                <p class="card-text">{{article.description}}...</p>
+                <a v-bind:href="article.url" target="_blank" class="btn btn-secondary btn-sm">Read More</a>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import Axios from 'axios'
+
+    export default{
+        data:function(){
+            return {
+                articles: ''
+            }
+        },
+        created:function(){
+            let vm = this;
+            Axios.get('https://newsapi.org/v1/articles?source=time&sortBy=latest&apiKey=245980c58e3a478eac2c71f8603dd229')
+                .then(function(response){
+                  vm.articles = response.data.articles;
+                })
+        }
+    }
+</script>
+
+<style>
+
+    img.card-img-top{
+        margin-bottom: 5px;
+    }
+
+    .row .card{
+        margin: 0 auto 2% auto;
+        padding: 0.5%;
+        text-align: center;
+    }
+
+    .card-block a{
+        margin: 0 auto;
+    }
+</style>
