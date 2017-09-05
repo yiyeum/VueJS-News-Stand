57aca301ee9a7e71000e006b2e3eb2def6f720d2
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-05 12:35:44 -0700

    Navigation updated

---
+++ src/Navigation.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <nav class="py-3 navbar navbar-expand-lg fixed-top row">
+            <div class="col">
+                <a class="nav-link" href="#">BBC</a>
+            </div>
+            <div class="col">
+                <a class="nav-link" href="#">CNN</a>
+            </div>
+            <div class="col">
+                <a class="nav-link" href="#">Time</a>
+            </div>
+            <div class="col">
+                <a class="nav-link" href="#">Google News</a>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default{}
+</script>
+
+<style>
+    nav {
+        background-color: lightsalmon;
+    }
+
+    nav a {
+        color: #fff;
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        font-size: 20px;
+        text-align: center;
+    }
+
+    nav a:hover{
+        color: darkgray;
+    }
+</style>
